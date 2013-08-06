fd5996a33b216fa7396054d97e20af96fd62ca80
Dustin Diep <dustindiep0@gmail.com>
2013-08-06 14:24:48 -0700

    Commit config
    This should commit it to the local repo, but should not push it up.

--- lib/config.php
+++ lib/config.php
@@ -2,8 +2,8 @@
 
 $dbConfig = array(
 	'dbhost' => 'localhost',
-	'database' => 'fhsapp_v2',
-	'username' => 'fhsapp_v2',
-	'password' => 'rainorshine4'
+	'database' => 'fhsapp',
+	'username' => 'root',
+	'password' => ''
 );
 
