0ebce28f3d9e7f3fa0015e22c703b2ec5f4c8940
Dustin Diep <dustindiep0@gmail.com>
2013-11-20 21:20:38 -0800

    The beginnings of create.php css

--- create.php
+++ create.php
@@ -2,7 +2,7 @@
 session_start(); 
 require_once('lib/config.php');
 require_once('lib/db.class.php');
-ini_set('display_errors', 1);
+ini_set('display_errors', 0);
 error_reporting(E_ALL);
 $db = new Db($dbConfig);
 require_once('functions.php');
@@ -104,112 +104,174 @@ $sports_p = $_SESSION['sports'];
 	);
 	</script>
 	<!--<link rel="stylesheet" href="style.css" />-->
+	<link rel="stylesheet" href="style.css" />
 	<link rel="stylesheet" href="css/ui-lightness/jquery-ui-1.10.3.custom.min.css" />
 </head>
 
 <body>
-	<div class="wrapper">
-		<form id="form" method="get" action="create.php">
+	<!--Header goes here-->
+	<div class="header">
+		<img class="logo" src="http://fhsapp.com/v2/Images/daytime.png">
+		
+		<div class="buttons">
+			 <a class="logout_button" href="logout.php">Log Out</a>
+		</div>	
+		
+		<div class="settings_button" >
+			<a href="settings.php"><img src="images/settings_gear.png" width="40" height="40"/></a>
+		</div>
+		
+		
+		
+		<div class="home_button_div">
+		 <a class="home_button" href="main.php?current=1">Home</a>
+		</div>
+		<?php
+		if($_SESSION['admin']) {
+			echo '<div class="new_user_button" >';
+			echo '<a href="new_user.php">Create New User</a><br />';
+			echo '</div>';
+		}
+		?>
+		<a href="create.php">
+			<div class="add_announcements_wrapper">
+			<div class="add_announcements_button">Add Announcement</div>
+			<img class="add_image" src="images/add.png" /> <!--Icons by DryIcons-->
+			</div>
+		</a>
+		
+	</div>	
+
+	<div class="create_wrapper">
+		<form id="form" method="get" action="create.php" class="anno_form">
 			<!--<label></label>
 			<input name="" type="text" value=""/>
 			<br />-->
-		
-			<label>Title:</label>
-			<input name="title" type="text" value="" />
-			<br />
+			<div class="anno_left">
+			<div class="anno_title">
+				<label class="anno_title_label">Title:</label>
+				<input name="title" type="text" value="" class="anno_text"/>
+				<br />
+			</div>
 			
-			<label>Description:</label>
-			<textarea name="description" rows="5" col="50"></textarea>
-			<br />
+			<div class="anno_description">
+				<label class="anno_description_label">Description:</label>
+				<textarea name="description" rows="5" col="50" class="anno_textarea"></textarea>
+				<br />
+			</div>
 			
-			<label>Announcement Starting Date:</label>
-			<input id="start_date" name="start_date" type="text" value=""/>
-			<br />
+			<div class="anno_start_end">
+				<div class="anno_start_date">
+					<label class="anno_start_date_label">Announcement Starting Date:</label>
+					<input id="start_date" name="start_date" type="text" value="" class="anno_text"/>
+					<br />
+				</div>
 			
-			<label>Announcement End Date:</label>
-			<input id="end_date" name="end_date" type="text" value=""/>
-			<br />
+				<div class="anno_end_date">
+					<label class="anno_end_date_label">Announcement End Date:</label>
+					<input id="end_date" name="end_date" type="text" value="" class="anno_text"/>
+					<br />
+				</div>
+			</div>
 			
-			<h3>Categories:</h3>
-			<!--php must generate these...-->
-			<?php
-			if($admin_p) {
-				$query = "SELECT * FROM subtype WHERE type_id = '1'";
-				$generals = $db->runQuery($query);
-				echo "<p>General:</p><br />";
-				foreach($generals as $general) {
-					$id = $general['id'];
-					$name = $general['name'];
-					if(!empty($name)) {
-						echo '<label>'.$name.':</label>
+			<div class="anno_optional">
+				<h3>Optional:</h3>
+				
+				<div class="anno_date">
+					<label class="anno_date_label">Actual Date of Event:</label>
+					<input id="date" name="date" type="text" value=""/>
+					<br />
+				</div>
+				
+				<div class="anno_time">
+					<label class="anno_time_label">Time of Event:</label>
+					<input name="time" type="text" value=""/>
+					<br />
+				</div>
+				
+				<div class="anno_location">
+					<label class="anno_location_label">Location:</label>
+					<input name="location" type="text" value=""/>
+					<br />
+				</div>
+			</div>
+			</div>
+			
+			<div class="anno_right">
+			<div class="anno_cats">
+				<h3>Categories:</h3>
+				<!--php must generate these...-->
+				<?php
+				if($admin_p) {
+					$query = "SELECT * FROM subtype WHERE type_id = '1'";
+					$generals = $db->runQuery($query);
+					echo "<div class='cat_div'><p class='cat_label'>General:</p><br />";
+					foreach($generals as $general) {
+						$id = $general['id'];
+						$name = $general['name'];
+						if(!empty($name)) {
+							echo '<label class="cat_subtype_label">'.$name.':</label>
+							<input name="check[]" type="checkbox" value="'.$id.'" />
+							<br />';
+						}
+					}
+					echo "</div>";
+				}
+				
+				if($teacher_p) {
+					$query = "SELECT * FROM subtype WHERE author_id='$user_id' AND type_id='2'";
+					$periods = $db->runQuery($query);
+					echo "<div class='cat_div'><p class='cat_label'>Classes:</p><br />";
+						foreach($periods as $period) {
+						$id = $period['id'];
+						$name = $period['name'];
+						$number = $period['period'];
+						if(!empty($name)) {
+							echo '<label class="cat_subtype_label">Period '.$number.': '.$name.'</label>
+							<input name="check[]" type="checkbox" value="'.$id.'" />
+							<br />';
+						}
+					}
+					echo "</div>";
+				}
+				
+				if($club_p) {
+					$query = "SELECT * FROM subtype WHERE author_id='$user_id' AND type_id='3'";
+					$clubs = $db->runQuery($query);
+					echo "<div class='cat_div'><p class='cat_label'>Club(s):</p><br />";
+					foreach($clubs as $club) {
+						$id = $club['id'];
+						$name = $club['name'];
+						echo '<label class="cat_subtype_label">'.$name.':</label>
 						<input name="check[]" type="checkbox" value="'.$id.'" />
 						<br />';
 					}
+					echo "</div>";
 				}
-			}
-			
-			if($teacher_p) {
-				$query = "SELECT * FROM subtype WHERE author_id='$user_id' AND type_id='2'";
-				$periods = $db->runQuery($query);
-				echo "<p>Classes:</p><br />";
-				foreach($periods as $period) {
-					$id = $period['id'];
-					$name = $period['name'];
-					$number = $period['period'];
-					if(!empty($name)) {
-						echo '<label>Period '.$number.': '.$name.'</label>
+				
+				if($sports_p) {
+					$query = "SELECT * FROM subtype WHERE author_id='$user_id' AND type_id='4'";
+					$sports = $db->runQuery($query);
+					echo "<div class='cat_div'><p class='cat_label'>Sport(s):</p><br />";
+					foreach($sports as $sport) {
+						$id = $sport['id'];
+						$name = $sport['name'];
+						echo '<label class="cat_subtype_label">'.$name.':</label>
 						<input name="check[]" type="checkbox" value="'.$id.'" />
 						<br />';
 					}
+					echo "</div>";
 				}
-			}
+				?>
+			</div>
 			
-			
-			if($club_p) {
-				$query = "SELECT * FROM subtype WHERE author_id='$user_id' AND type_id='3'";
-				$clubs = $db->runQuery($query);
-				echo "<p>Club(s):</p><br />";
-				foreach($clubs as $club) {
-					$id = $club['id'];
-					$name = $club['name'];
-					
-					echo '<label>'.$name.':</label>
-					<input name="check[]" type="checkbox" value="'.$id.'" />
-					<br />';
-				}
-			}
-			
-			if($sports_p) {
-				$query = "SELECT * FROM subtype WHERE author_id='$user_id' AND type_id='4'";
-				$sports = $db->runQuery($query);
-				echo "<p>Sport(s):</p><br />";
-				foreach($sports as $sport) {
-					$id = $sport['id'];
-					$name = $sport['name'];
-					
-					echo '<label>'.$name.':</label>
-					<input name="check[]" type="checkbox" value="'.$id.'" />
-					<br />';
-				}
-			}
-			?>
-			
-			<h3>Optional:</h3>
-			<label>Actual Date of Event:</label>
-			<input id="date" name="date" type="text" value=""/>
+			<div class="anno_submit">
+				<input type="submit" value="Create Announcement" />
+			</div>
 			<br />
-			
-			<label>Time of Event:</label>
-			<input name="time" type="text" value=""/>
+			</div>
+				
 			<br />
-			
-			<label>Location:</label>
-			<input name="location" type="text" value=""/>
-			<br />
-			
-			<input type="submit" value="Create Announcement" />
-			
-			<br /><a href="main.php">Back to Home</a><br />
 		</form>
 	</div>
 </body>
