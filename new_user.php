9c94223f6a32daecb58802f906f727245a3c7187
Dustin Diep <dustindiep0@gmail.com>
2013-11-20 20:01:04 -0800

    Fixed the goddamm indentation.

--- new_user.php
+++ new_user.php
@@ -1,4 +1,14 @@
-<?php session_start();?>
+<?php
+session_start();
+
+require_once('lib/config.php');
+require_once('lib/db.class.php');
+
+ini_set('display_errors', 0);
+error_reporting(E_ALL);
+
+$db = new Db($dbConfig);
+?>
 
 <!DOCTYPE HTML>
 
@@ -71,8 +81,6 @@
 		<div class="home_button_div">
 		 <a class="home_button" href="main.php">Home</a>
 		</div>
-
-	
 		
 		<a href="create.php">
 			<div class="add_announcements_wrapper">
@@ -82,114 +90,117 @@
 		</a>
 		
 	</div>	
-<?php
-
-require_once('lib/config.php');
-require_once('lib/db.class.php');
-
-ini_set('display_errors', 0);
-error_reporting(E_ALL);
-
-$db = new Db($dbConfig);
-?>
 
 	<div class="columns_wrapper">
 		<div class="columns">
 			<div class="column">
-
-		<?php
-			function checkbox_checked($checkbox_value) {
-				if($checkbox_value == "on") {
-					return 1;
-				} else {
-					return 0;
-				}
-			};
-			if(!empty($_REQUEST)) {
-				$username = $_REQUEST['username'];
-				$e_password = $_REQUEST['password']; //For emailing
-				$password = md5($_REQUEST['password']);
-				$first_name = $_REQUEST['first_name']; 
-				$last_name = $_REQUEST['last_name']; 
-				$email = $_REQUEST['email']; 
-				$admin = checkbox_checked($_REQUEST['admin']);
-				$teacher = checkbox_checked($_REQUEST['teacher']);
-				$club = checkbox_checked($_REQUEST['club']);
-				$sports = checkbox_checked($_REQUEST['sports']);
-				
-				//*Duplicate prevention
-				$already_exists = 0;
-				$existing_users = $db->runQuery('SELECT username FROM users;');
-				foreach($existing_users as $existing_user) {
-					if($existing_user['username'] == $username) {
-						$already_exists = 1;
-						break;
+			<?php
+				function checkbox_checked($checkbox_value) {
+					if($checkbox_value == "on") {
+						return 1;
+					} else {
+						return 0;
 					}
-				}
+				};
+				if(!empty($_REQUEST)) {
+					$username = $_REQUEST['username'];
+					$e_password = $_REQUEST['password']; //For emailing
+					$password = md5($_REQUEST['password']);
+					$first_name = $_REQUEST['first_name']; 
+					$last_name = $_REQUEST['last_name']; 
+					$email = $_REQUEST['email']; 
+					$admin = checkbox_checked($_REQUEST['admin']);
+					$teacher = checkbox_checked($_REQUEST['teacher']);
+					$club = checkbox_checked($_REQUEST['club']);
+					$sports = checkbox_checked($_REQUEST['sports']);
+					
+					//*Duplicate prevention
+					$already_exists = 0;
+					$existing_users = $db->runQuery('SELECT username FROM users;');
+					foreach($existing_users as $existing_user) {
+						if($existing_user['username'] == $username) {
+							$already_exists = 1;
+							break;
+						}
+					}
 				
-				if(!$already_exists) { //*Check if they exist.
-					if($admin||$teacher||$club||$sports) {	//*Has permission been selected?
-						$e_subject = "Fhsapp Username and Password";
-						$e_content = "Your login credentials for Fhsapp: \nUsername: $username\n\nPassword: $e_password";
-						$mail = mail($email, $e_subject, $e_content); //*EMAIL!!!
-						if($mail) {
-							//*Once the mail has worked
-							//*Create the user
-							$query = "INSERT into users(username, password, email, first_name, last_name, admin, teacher, club, sports) VALUES('$username', '$password', '$email', '$first_name', '$last_name', '$admin', '$teacher', '$club', '$sports');";
-							mysql_query($query);
-							echo "<p>New user has been created!</p>";
+					if(!$already_exists) { //*Check if they exist.
+						if($admin||$teacher||$club||$sports) {	//*Has permission been selected?
+							$e_subject = "Fhsapp Username and Password";
+							$e_content = "Your login credentials for Fhsapp: \nUsername: $username\n\nPassword: $e_password";
+							$mail = mail($email, $e_subject, $e_content); //*EMAIL!!!
+							if($mail) {
+								//*Once the mail has worked
+								//*Create the user
+								$query = "INSERT into users(username, password, email, first_name, last_name, admin, teacher, club, sports) VALUES('$username', '$password', '$email', '$first_name', '$last_name', '$admin', '$teacher', '$club', '$sports');";
+								mysql_query($query);
+								echo "<p>New user has been created!</p>";
+							} else {
+								echo "<p style='color:red;'>Email not valid.</p>";
+							}
 						} else {
-							echo "<p style='color:red;'>Email not valid.</p>";
+							echo "<p style='color:red;'>Please select a permission.</p>";
 						}
 					} else {
-						echo "<p style='color:red;'>Please select a permission.</p>";
+						echo "<p style='color:red;'>This user already exists!</p>";
 					}
-				} else {
-					echo "<p style='color:red;'>This user already exists!</p>";
-				}
-			}	
-		?>
+				}	
+			?>
 	
+				<form id="form" action="new_user.php" method="post" name="new_user_form">
+					<legend>
+						<center>
+							<h2>New User</h2>
+						</center>
+					</legend>
 
+					<div class="row">
+						<input name="first_name" onblur="if (this.value=='') this.value='First Name'" onfocus="if (this.value=='First Name') this.value = ''" type="text" value="First Name">
+					</div>
 		
-		
-<form id="form" action="new_user.php" method="post" name="new_user_form">
+					<div class="row"> 
+						<input name="last_name" onblur="if (this.value=='') this.value='Last Name'" onfocus="if (this.value=='Last Name') this.value = ''" type="text" value="Last Name">
+					</div> 
+					<!--<label>Last Name: </label><input type="text" name="last_name"/>-->
 
+					<div class="row"> 
+						<input name="email" onblur="if (this.value=='') this.value='E-Mail'" onfocus="if (this.value=='E-Mail') this.value = ''" type="text" value="E-Mail">
+					</div>
+					<!-- <label>E-Mail: </label><input type="text" name="e-mail"/>-->
 	
+					<div class="row">
+						<input name="username" onblur="if (this.value=='') this.value='Username'" onfocus="if (this.value=='Username') this.value = ''" type="text" value="Username">
+					</div> 
+					<!--<label>Username: </label><input type="text" name="username"/>-->
 
-	<legend><center><h2>New User</h2></center></legend>
+					<!--To solve the problem that the password type has to display the dots when clicked and that the box should also display non-dotted words before clicked: 
+					document.getElementById('password').focus() moves the curser from the first text box to the next and style="display: none" hides the second text box. onblur displays the origional text when we click something else-->
 
-	<div class="row"><input name="first_name" onblur="if (this.value=='') this.value='First Name'" onfocus="if (this.value=='First Name') this.value = ''" type="text" value="First Name"></div>
-	 
-	<div class="row"> <input name="last_name" onblur="if (this.value=='') this.value='Last Name'" onfocus="if (this.value=='Last Name') this.value = ''" type="text" value="Last Name"></div> <!--<label>Last Name: </label><input type="text" name="last_name"/>-->
+					<div class="row"> 
+						<input id="password_text" onfocus="this.style.display='none';document.getElementById('password').style.display='block'; document.getElementById('password').focus()" type="text" value="Password">
+						<input onblur="if (this.value==''){this.style.display='none';document.getElementById('password_text').style.display='block'}" id="password" style="display: none" type="password" name="password"/>
+					</div>
 
-	<div class="row"> <input name="email" onblur="if (this.value=='') this.value='E-Mail'" onfocus="if (this.value=='E-Mail') this.value = ''" type="text" value="E-Mail"></div><!-- <label>E-Mail: </label><input type="text" name="e-mail"/>-->
-	
-	<div class="row"><input name="username" onblur="if (this.value=='') this.value='Username'" onfocus="if (this.value=='Username') this.value = ''" type="text" value="Username"></div> <!--<label>Username: </label><input type="text" name="username"/>-->
+					<div class="row"> 
+						<input id="password_text2" onfocus="this.style.display='none';document.getElementById('password2').style.display='block'; document.getElementById('password2').focus()" type="text" value="Re-enter Password">
+						<input onblur="if (this.value==''){this.style.display='none';document.getElementById('password_text2').style.display='block'}" id="password2" style="display: none" type="password" name="password_2"/>
+					</div>
 
-<!--To solve the problem that the password type has to display the dots when clicked and that the box should also display non-dotted words before clicked: 
+					<div class="row inline_checkboxes">
+						<div id="categories">I Am A:<br/>
+							<input type="checkbox" name="teacher"/> Teacher
+							<input type="checkbox" name="coach"/> Coach 
+							<input type="checkbox" name="club_leader"/> Club Leader 
+						</div>
+					</div>
 
-		document.getElementById('password').focus() moves the curser from the first text box to the next and style="display: none" hides the second text box. onblur displays the origional text when we click something else-->
-
-	<div class="row"> <input id="password_text" onfocus="this.style.display='none';document.getElementById('password').style.display='block'; document.getElementById('password').focus()" type="text" value="Password"><input onblur="if (this.value==''){this.style.display='none';document.getElementById('password_text').style.display='block'}" id="password" style="display: none" type="password" name="password"/></div>
-
-	<div class="row"> <input id="password_text2" onfocus="this.style.display='none';document.getElementById('password2').style.display='block'; document.getElementById('password2').focus()" type="text" value="Re-enter Password"><input onblur="if (this.value==''){this.style.display='none';document.getElementById('password_text2').style.display='block'}" id="password2" style="display: none" type="password" name="password_2"/></div>
-
-
-		<div class="row inline_checkboxes">
-		<div id="categories">I Am A:<br/>
-		<input type="checkbox" name="teacher"/> Teacher
-		<input type="checkbox" name="coach"/> Coach 
-		<input type="checkbox" name="club_leader"/> Club Leader 
+					<div>
+						<input type="submit" value="Submit"/>
+					</div>
+				</form>
+			</div> <!--end column -->
 		</div>
-		</div>
-
-		<div><input type="submit" value="Submit"/></div>
-
-</form>
-	</div> <!--end column -->
-</div>
-</div>
+	</div>
 </body>
 
 </html>
