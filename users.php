bc5bccdd88f879d51523f3faa9ce655848e4edab
Dustin Diep <dustindiep0@gmail.com>
2014-01-11 13:14:57 -0800

    User stuff functions
    Needs CSS. Not my job. Go Julian Go.

--- users.php
+++ users.php
@@ -3,7 +3,7 @@ session_start();
 require_once('functions.php');
 include('lib/config.php');
 include('lib/db.class.php');
-ini_set('display_errors',1);
+ini_set('display_errors',0);
 error_reporting(E_ALL);
 $db = new Db($dbConfig);
 enforce_log();
@@ -29,6 +29,7 @@ $users=$db->runQuery($query);
 		<img class="beta" src="http://fhsapp.com/v2/Images/betterbeta.png">
 		<h1>FHS APP	</h1>
 		<div class="buttons">
+			 <a class="home_button" href="main.php?current=1">Home</a>
 			 <a class="logout_button" href="logout.php">Log Out</a>
 		</div>	
 		
@@ -53,11 +54,43 @@ $users=$db->runQuery($query);
 	</div>
 	
 	<div class="main_wrapper">
-		<pre>
+		<!--<pre>
 			<?php print_r($users);?>
-		</pre>
+		</pre>-->
 		
-		
+		<table border="1px border black" style="background-color:white">
+			<thead>
+				<tr> <!--Headers-->
+					<th>Last Name</th>
+					<th>First Name</th>
+					<th>Username</th>
+					<th></th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody>
+				<!--<tr>
+					<th><a href="edit_user.php?=1">Edit</a></th>
+					<th>Last name</th>
+					<th>First name</th>
+					<th>Username</th>
+					<th><a href="delete.user.php?=1">Delete</a></th>
+				</tr>-->
+				<?php 
+					foreach($users as $user) {
+						echo '
+						<tr>
+							<th>'.$user["last_name"].'</th>
+							<th>'.$user["first_name"].'</th>
+							<th>'.$user["username"].'</th>
+							<th><a href="edit_user.php?e_user_id='.$user["id"].'">Reset Password</a></th>
+							<th><a href="delete.user.php?d_user_id='.$user["id"].'">Delete</a></th>
+						</tr>';
+					}
+				?>
+			</tbody>
+		</table>
+
 	</div>
 </body>
 
