acdfbfa51b221096207489cbf7f7d0c9461e03d6
Dustin Diep <dustindiep0@gmail.com>
2014-03-08 14:25:30 -0800

    Form reconfiguration. Standby

---
+++ feedbackEmail.php
@@ -0,0 +1,9 @@
+<?php
+	$email = "fhsapp@gmail.com";
+	$grade = $_POST['grade'];
+	$feedback = $_POST['feedback'];
+	$e_subject = "Feedback";
+	$e_content = "Grade: $grade\n\nFeedback: $feedback";
+	$mail = mail($email, $e_subject, $e_content);
+	header('Location: http://www.fhsapp.com');
+?>
